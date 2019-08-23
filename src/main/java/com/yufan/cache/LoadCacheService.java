6f07f63b933afe16770cb17fd10f281986621248
lirf <512164882@qq.com>
2019-08-23 14:48:14 +0800

    代码更新

---
+++ src/main/java/com/yufan/cache/LoadCacheService.java
@@ -0,0 +1,11 @@
+package com.yufan.cache;
+
+/**
+ * 创建人: lirf
+ * 创建时间:  2019/3/26 16:47
+ * 功能介绍:
+ */
+//@Component
+public class LoadCacheService {
+
+}
---
+++ src/main/java/com/yufan/controller/InfoController.java
@@ -0,0 +1,15 @@
+package com.yufan.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 创建人: lirf
+ * 创建时间:  2019/8/23 14:44
+ * 功能介绍: 接口入口
+ */
+@RequestMapping(value = "info")
+public class InfoController {
+
+
+
+}
